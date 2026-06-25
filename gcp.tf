bc40dd280fbb8d45bfcc63b39bba3f5c33d6a397
Brad Sickles <brad.sickles@gmail.com>
2026-06-25 10:13:57 -0400

    enable apis

--- gcp.tf
+++ gcp.tf
@@ -3,3 +3,18 @@ data "google_project" "this" {}
 locals {
   project_id = data.google_project.this.project_id
 }
+
+# Gemini for Google Cloud
+resource "google_project_service" "cloudaicompanion" {
+  service                    = "cloudaicompanion.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
+
+
+# Gemini Cloud Assist
+resource "google_project_service" "geminicloudassist" {
+  service                    = "geminicloudassist.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
